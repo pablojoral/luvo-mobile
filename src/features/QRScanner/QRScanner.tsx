16566f12a724191ef3b3ab427f9ff1447e8c5525
Pablo Joral <pjoral@europe.altair.com>
2026-05-03 20:09:33 +0200

    fix: fix lint and typecheck.

--- src/features/QRScanner/QRScanner.tsx
+++ src/features/QRScanner/QRScanner.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useQRScanner } from 'stores/useQRScanner';
 import { QRScannerContent } from './components/QRScannerContent/QRScannerContent';
-import { useQRScannerComponent } from './hooks/useQRScannerComponent';
 
 export const QRScanner: React.FC = () => {
   const isOpen = useQRScanner(s => s.isOpen);
